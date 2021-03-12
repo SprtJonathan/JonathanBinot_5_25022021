bd4cb72a911a2ea29d65b2960908ed7eb7925b5d
Jonathan <jonathan.binot@gmail.com>
2021-03-12 15:26:59 +0100

    Ajout de la page produit et application de la sélection des options

--- oriphoto/js/index.js
+++ oriphoto/js/index.js
@@ -80,41 +80,7 @@ async function productsLibrary() {
     });
 };
 
-// Récupération du panier d'achat s'il existe sinon, il est créé
-
-/*let cartId = "userShoppingCart"
-
-if (localStorage.getItem(cartId)) {
-    console.log("The shopping cart exists")
-} else {
-    console.log("Shopping cart not found. Creating one...")
-    let shoppingCart = [];
-    localStorage.setItem(cartId, JSON.stringify(shoppingCart));
-};
-
-let shoppingCart = {
-    saveCart: function (product) {
-        let stringifiedCart = JSON.stringify(product);
-        localStorage.setItem(cartId, stringifiedCart);
-        return true;
-    },
-
-    getCart: function () {
-        return JSON.parse(localStorage.getItem(cartId));
-    },
-
-    clearCart: function () {
-        localStorage.removeItem(cartId);
-    }
-};*/
-
-// Affichage du panier
-/*console.log(shoppingCart.getCart.length);
-
-let shoppingCartNumberOfItems = document.getElementById("shopping-cart-number");
-shoppingCartNumberOfItems.innerHTML = shoppingCart.getCart.length;*/
-
-// Création de la page produit
+// Création de la page propre à chaque produit
 
 async function productDetailPage() {
 
@@ -124,6 +90,9 @@ async function productDetailPage() {
 
     // Appel à l'API pour récupérer les informations du produit
     const selectedProduct = await getProductsInfo();
+
+    //Ajout des éléments suivants au main de la page produit
+    let selectedProductDetails = document.getElementById("product-details-main");
 
     // Structure de la page de l'article
     // Container de l'article
@@ -137,46 +106,204 @@ async function productDetailPage() {
 
     // Légende de l'image
     let selectedProductArticle = document.createElement("article");
-    selectedProductArticle.setAttribute("class", "details--product-article");
+    selectedProductArticle.setAttribute("class", "details--product-article col-sm-12");
+
+    // Bloc regroupant le nom et la description de l'article
+    let selectedProductDetailsBlock = document.createElement("aside");
+    selectedProductDetailsBlock.setAttribute("class", "card details--product-details-aside");
 
     // Nom de l'article
     let selectedProductName = document.createElement("h3");
     selectedProductName.setAttribute("class", "details--product-name");
 
-    // Prix de l'article
-    let selectedProductPrice = document.createElement("h4");
-    selectedProductPrice.setAttribute("class", "details--product-price");
-
     // Description de l'article
     let selectedProductDescription = document.createElement("h5");
     selectedProductDescription.setAttribute("class", "details--product-description");
 
+    // Bloc regroupant les détails d'achat de l'article
+    let selectedProductBuyBlock = document.createElement("aside");
+    selectedProductBuyBlock.setAttribute("class", "card details--product-buy-aside");
+
+    // Prix de l'article
+    let selectedProductPrice = document.createElement("h4");
+    selectedProductPrice.setAttribute("class", "details--product-price");
+
+    // Texte pour les options de l'article
+    /*let selectedProductOptionsText = document.createElement("h6");
+    selectedProductOptionsText.setAttribute("class", "details--product-options-text");*/
+
     // Options de l'article
-    let selectedProductOptions = document.createElement("select");
-    selectedProductOptions.setAttribute("id", "product-options");
-    selectedProductOptions.setAttribute("class", "details--product-options");
+    let selectedProductSelect = document.createElement("select");
+    selectedProductSelect.setAttribute("id", "product-options");
+    selectedProductSelect.setAttribute("class", "details--product-options");
+    selectedProductSelect.setAttribute("onChange", "selectionChange(this);");
+
+    selectionChange = function (select) {
+        let selection = select.options[select.selectedIndex].text;
+        // Si l'index du select est 0 alors la réponse sera invalide
+        if (select.selectedIndex == 0) {
+            selectedProductSelect.setAttribute("value", "null");
+        } else {
+            selectedProductSelect.setAttribute("value", selection);
+        }
+    }
 
     // Lien pour acheter le produit
-    let selectedProductSelect = document.createElement("a");
-    selectedProductSelect.setAttribute("class", "btn btn-warning");
-    selectedProductSelect.setAttribute("href", "/cart.html");
+    let selectedProductBuy = document.createElement("a");
+    selectedProductBuy.setAttribute("id", "add-to-cart-button");
+    selectedProductBuy.setAttribute("class", "btn btn-warning");
+    selectedProductBuy.setAttribute("href", "/cart.html");
+    selectedProductBuy.setAttribute("onclick", "addToCart()");
 
     /* Configuration de la parenté de chaque élément: 
     Le container se retrouve sous la section contenant les articles, l'image et la légende dans le figure, et les caractéristiques dans le figcaption*/
     selectedProductDetails.appendChild(selectedProductContainer);
     selectedProductContainer.appendChild(selectedProductImage);
-    selectedProductContainer.appendChild(selectedProductFigcaption);
-    selectedProductFigcaption.appendChild(selectedProductName);
-    selectedProductFigcaption.appendChild(selectedProductPrice);
-    selectedProductFigcaption.appendChild(selectedProductSelect);
+    selectedProductContainer.appendChild(selectedProductArticle);
+    selectedProductArticle.appendChild(selectedProductDetailsBlock);
+    selectedProductArticle.appendChild(selectedProductBuyBlock);
+    selectedProductDetailsBlock.appendChild(selectedProductName);
+    selectedProductDetailsBlock.appendChild(selectedProductDescription);
+    selectedProductBuyBlock.appendChild(selectedProductPrice);
+    //selectedProductBuyBlock.appendChild(selectedProductOptionsText);
+    selectedProductBuyBlock.appendChild(selectedProductSelect);
+    selectedProductBuyBlock.appendChild(selectedProductBuy);
+
+    document.getElementById("product-name").textContent = selectedProduct.name;
+    document.title = "Produit : " + selectedProduct.name;
+
+    // Création des options du produit
+    // Option par défaut : Texte disant de choisir (retournant une value de null)
+    const defaultSelectedProductOption = document.createElement("option");
+    selectedProductSelect.appendChild(defaultSelectedProductOption);
+    defaultSelectedProductOption.innerHTML = "Choisissez votre option";
+
+    // Boucle 'for' créant les options à partir de l'api
+    let soldProductOptions = selectedProduct.lenses;
+
+    for (let i = 0; i < soldProductOptions.length; i++) {
+        let selectedProductOption = document.createElement("option");
+        selectedProductOption.innerHTML = selectedProduct.lenses[i];
+        selectedProductSelect.appendChild(selectedProductOption);
+        selectedProductSelect.setAttribute("value", "null");
+        console.log(selectedProduct.lenses[i], i);
+    }
+
 
     // Contenu de chaque balise créée
     selectedProductName.textContent = selectedProduct.name;
     selectedProductPrice.textContent = parseFloat(selectedProduct.price / 100).toFixed(2) + " €";
-    selectedProductSelect.textContent = "Ajouter au panier";
+    selectedProductDescription.textContent = selectedProduct.description;
+    selectedProductBuy.textContent = "Ajouter au panier";
+    selectedProductBuy.addEventListener("click", function (event) {
+        event.preventDefault()
+    });
 };
 
+// Création du script du panier
+
+/*let shoppingCart = (function () {
+    let cart = [];
+
+    function Item(name, id, price, count) {
+        this.name = selectedProduct.name;
+        this.id = selectedProduct.id;
+        this.price = selectedProduct.price;
+        this.count = count;
+    }
+
+    function saveCart() {
+        localStorage.setItem(cartId, JSON.stringify(cart));
+    }
+
+    function loadCart() {
+        cart = JSON.parse(localStorage.getItem(cartId));
+        if (cart === null) {
+            cart = []
+        }
+    }
+
+    loadCart();
+
+    let selectedItem = {};
+
+    selectedItem.addItemToCart = function (name, id, price, count) {
+        for (let i in cart) {
+            if (cart[i].name === selectedProduct.name) {
+                cart[i].count += count;
+                saveCart();
+                return;
+            }
+        }
+
+        console.log("addItemToCart", name, id, price, count)
+    }
+})*/
+/*
+let checkShoppingCart = () => {
+    if (localStorage.getItem(cartId)) {
+        isShoppingCart = true;
+    }
+}
+
+if (isShoppingCart = false) {
+
+}*/
+
+// Configuration du bouton ajouter au panier
+async function addToCart() {
+
+    // Appel à l'API pour récupérer les informations du produit
+    const selectedProduct = await getProductsInfo();
+
+    let addToCartButton = document.getElementById("add-to-cart-button");
+    document.addEventListener("click", addToCartButton)
+
+    // Ajout au panier
+
+    //Déclaration des variables du panier
+    const cartId = "userShoppingCart" // Nom du panier qui sera ajouté au localStorage
+    let isShoppingCart = false; // Le panier est-il initialisé
+    let shoppingCart = []; // Tableau contenant les éléments ajoutés au panier
+
+    const itemName = selectedProduct.name;
+    //console.log(itemName);
+
+    const itemId = selectedProduct._id;
+    //console.log(itemId);
+
+    const itemPrice = parseFloat(selectedProduct.price / 100).toFixed(2) + " €";
+    //console.log(itemPrice);
+
+    // Option sélectionnée
+    let select = document.getElementById("product-options");
+    let itemOption = select.value;
+    let cartItemsNumber = shoppingCart.length;
+    let itemCount = 1;
+
+    for (var i = 0; i < cartItemsNumber; i++) {
+        if (cartItemsNumber[i].itemId == itemId) {
+            itemCount += itmenCount;
+        }
+    }
+    let selectedItem = { itemName, itemId, itemPrice, itemOption, itemCount }
+    // Si aucune option n'est choisie, alors on retourne une erreur
+    if (itemOption != "Choisissez votre option") {
+        shoppingCart.push(selectedItem);
+    } else {
+        alert("Erreur : Veuillez sélectionner une option");
+    }
+    console.log(itemCount);
 
 
+
+
+
+
+
+
+
+
+}
 
 
